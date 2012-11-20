e6e8b869bb7ecd75fb3e9c4174d7eb83e19424a6
Thomas Monzel <tom.monzel@gmail.com>
2012-11-20 23:27:49 +0100

    Added new routing namespace functions

--- lib/http/application/routes.php
+++ lib/http/application/routes.php
@@ -8,8 +8,44 @@ namespace http\application\routes {
     return $scope;
   }
   
+  function scope($locals, $block = null) {
+    base()->scope($locals, $block);
+  }
+  
+  function resource($name, $locals = array(), $block = null) {
+    base()->resource($name, $locals, $block);
+  }
+  
+  function resources() {
+    call_user_func_array(array(base(), 'resources'), func_get_args());
+  }
+  
+  function controller($name, $locals, $block = null) {
+    base()->controller($name, $locals, $block);
+  }
+  
   function to($target) {
     base()->to($target);
+  }
+  
+  function match($conditions, $options, $target = null) {
+    base()->match($conditions, $options, $target);
+  }
+  
+  function get($paths, $target, array $options = array()) {
+    base()->get($paths, $target, $options);
+  }
+  
+  function post($paths, $target, array $options = array()) {
+    base()->post($paths, $target, $options);
+  }
+  
+  function put($paths, $target, array $options = array()) {
+    base()->put($paths, $target, $options);
+  }
+  
+  function delete($paths, $target, array $options = array()) {
+    base()->delete($paths, $target, $options);
   }
   
   function local($name, $value) {
